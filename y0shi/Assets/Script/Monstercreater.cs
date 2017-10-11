2b5c52e3d79f43f024592d906fa08703fb540175
miyauchiryouma <ryoma.riruru@gmail.com>
2017-10-11 14:16:43 +0900

    10_11
    10_11

---
+++ y0shi/Assets/Script/Monstercreater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstercreater : MonoBehaviour {
+
+	public Transform monster1;
+	public Transform monster2;
+	public Transform monster3;
+	void Start () {
+		
+	}
+
+	void Update () {
+			GameObject obj = getClickObject ();
+		if (obj != null) {
+		/*	Vector3 pos = new Vector3(obj.transform.position.x, obj.transform.position.y, obj.transform.position.z+2);
+			Quaternion rote = Quaternion.Euler(0, 0, 0);
+			Instantiate (monster1, pos, rote);
+		*/
+			Destroy (obj);
+				}
+		}
+	private GameObject getClickObject() {
+		GameObject result = null;
+		if(Input.GetMouseButtonDown(0)) {
+			Vector3 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+			if (collition2d) {
+				result = collition2d.transform.gameObject;
+				}
+			}
+		return result;
+	}
+
+}
---
+++ y0shi/Assets/Script/Roomcreater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roomcreater : MonoBehaviour {
+		public Transform room1;
+		public Transform room2;
+		public Transform room3;
+	    public Transform room4;
+	    int z = 10;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = new Vector3(this.transform.position.x, this.transform.position.y+z ,this.transform.position.z);
+		Quaternion rote = Quaternion.Euler(0, 0, 0);
+		if (Input.GetKeyDown (KeyCode.C)) {
+			Instantiate (room1, pos, rote);
+			z = z + 10;
+		}
+	}
+
+}
